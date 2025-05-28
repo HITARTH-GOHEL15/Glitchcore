9b8dd911cfd0b08974f223d062a1dc8a4458b8a5
Hitarth gohel <hitarthgohel15@gmail.com>
2025-05-28 11:46:14 +0530

    font file added

---
+++ app/src/main/java/com/example/hd_1/ui/theme/fontFamily.kt
@@ -0,0 +1,37 @@
+package com.example.hd_1.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.hd_1.R
+
+val sora_bold = FontFamily(
+    Font(R.font.sora_bold)
+)
+
+val sora_extraBold = FontFamily(
+    Font(R.font.sora_extrabold)
+)
+
+val sora_extraLight = FontFamily(
+    Font(R.font.sora_extralight)
+)
+
+val sora_light = FontFamily(
+    Font(R.font.sora_light)
+)
+
+val sora_medium = FontFamily(
+    Font(R.font.sora_medium)
+)
+
+val sora_regular = FontFamily(
+    Font(R.font.sora_regular)
+)
+
+val sora_semibold = FontFamily(
+    Font(R.font.sora_semibold)
+)
+
+val sora_thin = FontFamily(
+    Font(R.font.sora_thin)
+)
--- app/src/main/java/com/example/hd_1/user_interface/HomeScreen.kt
+++ app/src/main/java/com/example/hd_1/user_interface/HomeScreen.kt
@@ -1,2 +1,27 @@
 package com.example.hd_1.user_interface
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "GlitchCore",
+                    )
+                }
+            )
+        }
+    ) {
+
+    }
+}
